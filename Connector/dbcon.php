5c4b2f75c5e4c9711ff07dbd756c3f95cc0aba1d
Scrap-H <mervin.chung729@gmail.com>
2024-04-15 05:41:19 +0100

    start of db connection

--- Connector/dbcon.php
+++ Connector/dbcon.php
@@ -1,16 +1,35 @@
 <?php
 
-$serverLink = "croptech.database.windows.net";
-$name = "UserDetail";
-$username =  "CroptechAdmin";
-$password = "Crop#Tech~Admin$";
+$host = 'croptech-server.mysql.database.azure.com';
+$username = 'xciernemex';
+$password = 'ZRWCk$5wSb26ma2X';
+$db_name = 'your_database';
 
-try {
-    $conn = new PDO("sqlsrv:server = tcp:croptech.database.windows.net,1433; Database = UserDetail", "CroptechAdmin", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
