287ebcca86370fab9224923dee1e1fac9027a6f0
Scrap-H <mervin.chung729@gmail.com>
2024-05-03 02:46:21 +0100

    model box fix and addition + model content structure planning

--- js/model.js
+++ js/model.js
@@ -1,14 +1,16 @@
-function boxtoggle(initialBoxId, expandedBoxId) {
-
+function boxtoggle(initialBoxId, expandedBoxId, expandBackgroundId) {
+    
     var initialBox = document.getElementById(initialBoxId);
     var expandedBox = document.getElementById(expandedBoxId);
+    var expandBackground = document.getElementById(expandBackgroundId);
 
     if (initialBox.style.display === 'block') {
         initialBox.style.display = 'none';
         expandedBox.style.display = 'block';
+        expandBackground.style.display = 'block';
     } else {
         initialBox.style.display = 'block';
         expandedBox.style.display = 'none';
+        expandBackground.style.display = 'none';
     }
-    
 }
--- pages/dashboard2.php
+++ pages/dashboard2.php
@@ -83,7 +83,8 @@ echo '<h1> Hello '.$name. '</h1>';
     <h1 id="SensorVal1"></h1>
 </div>
 
-<div onclick="boxtoggle('Soil2' , 'Soil1')" class="expandedbox" id = "Soil2">
+<div  onclick="boxtoggle('Soil2' , 'Soil1' , 'solid3')" class = "ExpandBackground" id = "Soil2">
+<div class="expandedbox" id = "solid3" >
     <img width="100" height="100" src="https://img.icons8.com/ios/100/soil.png" alt="soil"/>
     <div class="moistureBox">
         <div>
@@ -108,21 +109,37 @@ echo '<h1> Hello '.$name. '</h1>';
                 <h1>dummy</h1>
             </div>
         </div>
+
+        <div>
+        <p>Status : </p>
+    <div>GREEN</div>
+    <div>YELLOW</div>
+    <div>RED</div>
+    </div>
+    
+
+
+
     </div>
 </div>
+</div>
+
+
+<!-- INCLUDE CONNECTION BUTTON FOR ARDUINO / RASBERRY PIE -->
 
 <!-- Weather -->
 
 <div onclick="boxtoggle('Weather1' , 'Weather2')" class="initialbox" id = "Weather1">
 
-<img width="32" height="32" src="https://img.icons8.com/windows/32/chance-of-storm--v1.png" alt="chance-of-storm--v1"/>
+<img width="90" height="90" src="https://img.icons8.com/ios-glyphs/90/chance-of-storm.png" alt="chance-of-storm"/>
 <h2>Forecast</h2>
 <h1 id="SensorVal1"></h1>
 </div>
 
-<div onclick="boxtoggle('Weather2' , 'Weather1')" class = "expandedbox" id = "Weather2">
+<div onclick="boxtoggle('Weather2' , 'Weather1' , 'Weather3')" class = "ExpandBackground" id = "Weather2">
+<div  class = "expandedbox" id = "Weather3" >
 
-<img width="100" height="100" src="https://img.icons8.com/ios/100/soil.png" alt="soil"/>
+<img width="90" height="90" src="https://img.icons8.com/ios-glyphs/90/chance-of-storm.png" alt="chance-of-storm"/>
 <div class = "moistureBox">
 
     <div>
@@ -150,7 +167,131 @@ echo '<h1> Hello '.$name. '</h1>';
 
 </div>
 
+</div>
+
+<!-- ADD FILLABLE TYPES SO THAT USERS CAN ENTER THEIR LOCATION FOR ACCURATE FORECAST BUT ALSO HAVE METHODS TO PREVENT THEM FROM ENTERING EXACT LOCATIONS IF THEY DON'T CHOOSE TO -->
+
+
+<!-- Statistics -->
+
+<div onclick="boxtoggle('Statistics1' , 'Statistics2')" class="initialbox" id = "Statistics1">
+
+<img width="100" height="100" src="https://img.icons8.com/ios/100/combo-chart--v1.png" alt="combo-chart--v1"/>
+<h2>Statistics</h2>
+</div>
+
+<div onclick="boxtoggle('Statistics2' , 'Statistics1' , 'Statistics3')" class = "ExpandBackground" id = "Statistics2">
+<div  class = "expandedbox" id = "Statistics3" >
+
+<img width="90" height="90" src="https://img.icons8.com/ios-glyphs/90/chance-of-storm.png" alt="chance-of-storm"/>
+<div class = "moistureBox">
+
+    <div>
+    <h1>Weather Forecast </h1>
+    <h1 id="SensorVal2"></h1>
+    </div>
+
+    <div class = "sensorDividor">
+
+    <div class = "sensorContent">
+        <h1> DISPLAY CHART  </h1>
+    </div>
+
+    </div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<!-- Switch -->
+
+<div onclick="boxtoggle('Switch1' , 'Switch2')" class="initialbox" id = "Switch1">
+
+<img width="100" height="100" src="https://img.icons8.com/ios/100/switches.png" alt="switches"/>
+<h2>Switch</h2>
+</div>
+
+<div onclick="boxtoggle('Switch2' , 'Switch1' , 'Switch3')" class = "ExpandBackground" id = "Switch2">
+<div  class = "expandedbox" id = "Switch3" >
+
+<img width="100" height="100" src="https://img.icons8.com/ios/100/switches.png" alt="switches"/>
+<div class = "moistureBox">
+
+    <div>
+    <h1>Weather Forecast </h1>
+    <h1 id="SensorVal2"></h1>
+    </div>
+
+    <div class = "sensorDividor">
+
+    <div>
+        <p>Status : </p>
+    <div>GREEN</div>
+    <div>YELLOW</div>
+    <div>RED</div>
+    </div>
+    
+
+    <div class = "sensorContent">
+        <h1> Irrigation Switch  </h1>
+        <button>On</button>
+    </div>
+
+
+    <!-- Connect Irrigation same as sensors -->
+
+    </div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<!-- Automation -->
+
+<div onclick="boxtoggle('Automation1' , 'Automation2')" class="initialbox" id = "Automation1">
+
+<img width="100" height="100" src="https://img.icons8.com/ios/100/robot-3.png" alt="robot-3"/>
+<h2>Automation</h2>
+</div>
+
+<div onclick="boxtoggle('Automation2' , 'Automation1' , 'Automation3')" class = "ExpandBackground" id = "Automation2">
+<div  class = "expandedbox" id = "Automation3" >
+
+<img width="100" height="100" src="https://img.icons8.com/ios/100/robot-3.png" alt="robot-3"/>
+<div class = "moistureBox">
+
+    <div>
+    <h1>Weather Forecast </h1>
+    <h1 id="SensorVal2"></h1>
+    </div>
+
+    <div class = "sensorDividor">
+
+    <div class = "sensorContent">
+        <h1> Display timetable of Automation  </h1>
+    </div>
+
+
+    <!-- Connect Irrigation same as sensors -->
+
+    </div>
+
+</div>
+
+</div>
+
+</div>
+
+
+
 <!-- Logout -->
+
 
 <div class = "initialbox">
 
